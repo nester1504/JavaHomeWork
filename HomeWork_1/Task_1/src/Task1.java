da939c8c15988abea40a73e667119ccafa221b46
Alexandr Nesterenko <nesteronok@gmail.com>
2023-06-08 00:54:33 +0600

    Task 1 Finish

---
+++ HomeWork_1/Task_1/src/Task1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void ScanNumberN() {
+        System.out.print("Введите число n = ");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(SumOfNumbersToN(n));
+    }
+
+    public static int SumOfNumbersToN(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+}
