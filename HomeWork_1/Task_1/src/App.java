ee1a972a08d0d948006ab349e1a8685f9cef663b
Alexandr Nesterenko <nesteronok@gmail.com>
2023-06-08 00:53:38 +0600

    Task 1 Finish

--- HomeWork_1/Task_1/src/App.java
+++ HomeWork_1/Task_1/src/App.java
@@ -1,20 +1,9 @@
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.print("Введите число n = ");
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        System.out.println(SumOfNumbersToN(n));
+        Task1.ScanNumberN();
 
     }
 
-    private static int SumOfNumbersToN(int n) {
-        int sum = 0;
-        for (int i = 1; i < n; i++) {
-            sum = sum + i;
-        }
-        return sum;
-    }
 }
