27dd704c54eb7e6afce9f1d8748f1a99c2fe3e1b
Alexandr Nesterenko <nesteronok@gmail.com>
2023-06-15 23:32:52 +0600

    HomeWork2 Task 1

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -6,24 +6,27 @@ import java.util.logging.Level;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {5, 2, 8, 12, 3, 7, 45, 2, 5, 7};
 
-        try {
-            FileHandler fileHandler = new FileHandler("log.txt");
-            fileHandler.setLevel(Level.INFO);
+        Task1.sQLQueryBuilder();
 
-            Task2.logger.addHandler(fileHandler);
+        // int[] array = {5, 2, 8, 12, 3, 7, 45, 2, 5, 7};
 
-            Task2.logger.setLevel(Level.INFO);
+      //  try {
+       //     FileHandler fileHandler = new FileHandler("log.txt");
+       //     fileHandler.setLevel(Level.INFO);
 
-            Task2.logger.info("Исходный массив: " + Arrays.toString(array));
+       //     Task2.logger.addHandler(fileHandler);
 
-            Task2.bubbleSort(array);
+        //    Task2.logger.setLevel(Level.INFO);
 
-            Task2.logger.info("Отсортированный массив: " + Arrays.toString(array));
-        } catch (IOException e) {
-            System.out.println("Ошибка при записи логов в файл: " + e.getMessage());
-        }
+       //     Task2.logger.info("Исходный массив: " + Arrays.toString(array));
+
+        //    Task2.bubbleSort(array);
+
+        //    Task2.logger.info("Отсортированный массив: " + Arrays.toString(array));
+       // } catch (IOException e) {
+        //    System.out.println("Ошибка при записи логов в файл: " + e.getMessage());
+      //  }
 
 
     }
--- HomeWork2/src/Task1.java
+++ HomeWork2/src/Task1.java
@@ -1,3 +1,37 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Task1 {
 
+    public static void sQLQueryBuilder() {
+        String filterParams = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        StringBuilder whereClause = new StringBuilder();
+
+        // Удаляем возможные пробелы в строке параметров
+
+        filterParams = filterParams.replaceAll("\\s+", "");
+
+        // Удаляем первую и последнюю фигурные скобки из строки параметров
+        filterParams = filterParams.substring(1, filterParams.length() - 1);
+
+        String[] params = filterParams.split(",");
+
+        for (String param : params) {
+            String[] keyValue = param.split(":");
+            String key = keyValue[0].replaceAll("\"", "");
+            String value = keyValue[1].replaceAll("\"", "");
+
+            if (!value.equals("null")) {
+                if (whereClause.length() > 0) {
+                    whereClause.append(" AND ");
+                }
+                whereClause.append(key).append(" = '").append(value).append("'");
+            }
+        }
+
+        String sqlQuery = "SELECT * FROM students WHERE " + whereClause.toString();
+        System.out.println(sqlQuery);
+
+    }
 }
