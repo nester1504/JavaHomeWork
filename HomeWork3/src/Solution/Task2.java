d69f314f8166b7ace0c2727f0929029e1f87d60a
Alexandr Nesterenko <nesteronok@gmail.com>
2023-06-18 00:41:17 +0600

    Task 2

--- HomeWork3/src/Solution/Task2.java
+++ HomeWork3/src/Solution/Task2.java
@@ -1,4 +1,31 @@
 package Solution;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Task2 {
+
+    public static List<Integer> randomList(){
+        Random rd = new Random();
+        List<Integer> integerList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            integerList.add(rd.nextInt(100));
+        }
+        System.out.println(integerList);
+        return integerList;
+    }
+
+    public static void removalOfEven(List<Integer> list)
+    {
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) % 2 == 0)
+            {
+                list.remove(i);
+            }
+        }
+        System.out.println(list);
+    }
+
 }
--- HomeWork3/src/main.java
+++ HomeWork3/src/main.java
@@ -1,5 +1,12 @@
+import Solution.Task2;
+
+import java.util.List;
+
 public class main {
     public static void main(String[] args) {
+
+        List<Integer> list = Task2.randomList();
+        Task2.removalOfEven(list);
 
     }
 }
