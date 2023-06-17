3b34489d4842cfbeddd194a751739c8be648280f
Alexandr Nesterenko <nesteronok@gmail.com>
2023-06-18 00:19:15 +0600

    Project 3

--- HomeWork2/src/Main.java
+++ HomeWork2/src/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,25 +13,26 @@ public class Main {
 
         // int[] array = {5, 2, 8, 12, 3, 7, 45, 2, 5, 7};
 
-      //  try {
-       //     FileHandler fileHandler = new FileHandler("log.txt");
-       //     fileHandler.setLevel(Level.INFO);
+        //  try {
+        //     FileHandler fileHandler = new FileHandler("log.txt");
+        //     fileHandler.setLevel(Level.INFO);
 
-       //     Task2.logger.addHandler(fileHandler);
+        //     Task2.logger.addHandler(fileHandler);
 
         //    Task2.logger.setLevel(Level.INFO);
 
-       //     Task2.logger.info("Исходный массив: " + Arrays.toString(array));
+        //     Task2.logger.info("Исходный массив: " + Arrays.toString(array));
 
         //    Task2.bubbleSort(array);
 
         //    Task2.logger.info("Отсортированный массив: " + Arrays.toString(array));
-       // } catch (IOException e) {
+        // } catch (IOException e) {
         //    System.out.println("Ошибка при записи логов в файл: " + e.getMessage());
-      //  }
+        //  }
 
 
-        Task4.calculator();
+        // Task4.calculator();
+
 
     }
 }
